44e64177948b7db2b546b8986ee5636f52adce7c
Milan Zítka <milan@digitalvision.cz>
2024-12-03 16:28:39 +0100

    chore: add docker container for Storybok

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -35,6 +35,10 @@ export default {
                     },
                 }),
             ],
+            server: {
+                host: true,
+                open: false,
+            },
         });
     },
     docs: {},
